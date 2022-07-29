bd7232511271f7d8677bb33eaa67a48b71eba124
sidhan-shamil <sidhanshamil1@gmail.com>
2022-07-29 14:39:38 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './modules/about/about.component';
+import { ContactComponent } from './modules/contact/contact.component';
+import { HomeComponent } from './modules/home/home.component';
+import { MenuComponent } from './modules/menu/menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'menu', component: MenuComponent},
+  {path: 'about', component: AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './defaultLayout/navbar/navbar.component';
+import { FooterComponent } from './defaultLayout/footer/footer.component';
+import { HomeComponent } from './modules/home/home.component';
+import { AboutComponent } from './modules/about/about.component';
+import { MenuComponent } from './modules/menu/menu.component';
+import { ContactComponent } from './modules/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    MenuComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
